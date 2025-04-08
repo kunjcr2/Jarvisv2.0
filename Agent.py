babfdf1d2fb5c64f227010c7d5a9de01136c874e
Kunj Shah <159096079+kunjcr2@users.noreply.github.com>
2025-04-07 17:41:59 -0700

    llm changed in Agent

--- Agent.py
+++ Agent.py
@@ -1,13 +1,16 @@
 from langchain.agents import initialize_agent, Tool, AgentType
 import Tools
 
+from llm import get_llm
+import os
+
 tool = Tools.ToolManager()
 
 # creating the agent
 def get_agent():
     agent = initialize_agent(
         tools=tool.get_tools(),
-        llm=Tools.get_llm(),
+        llm=get_llm(api_key=os.getenv('OPENAI_API_KEY')),
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         agent_name="pie-assistant",
         verbose=True,
@@ -20,4 +23,3 @@ def get_ans():
     agent = get_agent()
     ans = agent.run("What is the capital of France?")
     return ans
-
